4e71e152708bca759b6527ee33022a7a4cddc6f7
Finn H <finn.hoffhenke@gmx.de>
2021-08-09 18:26:19 +0200

    [+] Added Maven Build

---
+++ src/main/java/src/loxleyshadow/healworld/classifier/ChunkClassifier.java
@@ -0,0 +1,30 @@
+package loxleyshadow.healworld.classifier;
+
+import java.util.List;
+
+import org.bukkit.Chunk;
+import org.bukkit.Material;
+
+import loxleyshadow.healworld.HealWorldMain;
+
+public class ChunkClassifier {
+
+	private static int devThreshold;
+	
+	public ChunkClassifier(int threshold) {
+		devThreshold = threshold;
+	}
+	
+	public boolean isDeveloped(Chunk chunk) {
+		List<String> playerBlocks = HealWorldMain.getInstance().getConfig().getStringList("playerBlocks");
+		int devCount = 0;
+        
+        for (String str : playerBlocks) {
+        	if (chunk.contains(Material.getMaterial(str).createBlockData())) {
+        		devCount++;
+        	}
+        }
+        
+        return devCount >= devThreshold;
+	}
+}
